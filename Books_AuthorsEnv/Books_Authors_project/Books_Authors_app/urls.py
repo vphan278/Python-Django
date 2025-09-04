ade046b8a09e2285b4a437a7288359ab121c7b02
vphan278 <vphan2662@gmail.com>
2025-09-03 19:43:15 -0700

    Delete Books_AuthorsEnv directory

--- Books_AuthorsEnv/Books_Authors_project/Books_Authors_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('books/create', views.add_item_to_inventory),
-    path('authors/create', views.add_author),
-]
